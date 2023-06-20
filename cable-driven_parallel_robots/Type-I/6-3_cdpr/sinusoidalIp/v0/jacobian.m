685537c87bc51d13ad6da7ab17830a126bf39029
Teja Krishna Mamidi <135995908+TejaKrishnaMamidi@users.noreply.github.com>
2023-06-21 00:07:47 +0530

    Add files via upload

---
+++ cable-driven_parallel_robots/Type-I/6-3_cdpr/sinusoidalIp/v0/jacobian.m
@@ -0,0 +1,27 @@
+% RSSLM-CDPR-Type-I (or Kinetostatics) jacobian module. This code decides when to update the constraint Jacobain matrix and it's derivative.
+
+% Contributors: Dr. Teja Krishna Mamidi, Prof. Sandipan Bandyopadhyay @IIT Madras, 
+% Acknowledgments: Dr. Suril V. Shah and Prof. S. K. Saha @IIT Delhi
+
+% Function calls to jacobian_init.m
+
+% System: 6-3 CDPR with (sinusoidal or linear) feed
+
+function []=jacobian()
+
+% Global variables -- required
+global qi dqi q dq;
+
+dpos = max(abs(qi-q));
+dvel = max(abs(dqi-dq));
+
+%disp([dpos,dvel]);
+
+% If changes in any of the joint positions or velocities are more than the below tolerance, then the constraint Jacobian matirx is updated.  
+tol=1e-5;
+
+if dpos >=tol || dvel >=tol
+    jacobian_init()
+end
+
+end
